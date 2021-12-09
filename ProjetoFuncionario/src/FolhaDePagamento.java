a965d7632dba6979dbcb7a7980b764053b803ad8
SidneySilvaAraujo <sidneiaraujo219@gmail.com>
2021-12-09 09:10:33 -0300

    Introdusidos geters & seters

--- ProjetoFuncionario/src/FolhaDePagamento.java
+++ ProjetoFuncionario/src/FolhaDePagamento.java
@@ -1,3 +1,4 @@
+import nucleo.Funcionario;
 
 public class FolhaDePagamento {
 	public static void main(String[] args) {
@@ -6,26 +7,27 @@ public class FolhaDePagamento {
 		f2 = new Funcionario();
 		f3 = new Funcionario();
 		//primeiro funcionário;
-		f1.numeroFuncional = 1234;
-		f1.nome = "Sidney da Silva Araujo Silverio";
-		f1.cargo = "reitor";
-		f1.salario = 17000.0;
+		f1.setNumeroFuncional(1234);
+		f1.setNome("Sidney da Silva Araujo Silverio");
+		f1.setCargo("Reitor");
+		f1.setSalario(2000.0);
 		//segundo funcionário;
-		f2.numeroFuncional = 5678;
-		f2.nome = "Rogério Serafin de Matos";
-		f2.cargo = "reitor";
-		f2.salario = 17000.0;
+		f2.setNumeroFuncional(5678);
+		f2.setNome("Rogério Serafin de Matos");
+		f2.setCargo("Analista de Engenharia de ti senhor");
+		f2.setSalario(12000.0);
 		//terseiro funcionário;
-		f3.numeroFuncional = 11221345;
-		f3.nome = "Natalia da Silva Araujo Silverio";
-		f3.cargo = "Engenheira de software";
-		f3.salario = 12000.0;
-		//objetos da classe Funcionario definidos;
+		f3.setNumeroFuncional(12213412);
+		f3.setNome("Natália da silva Araujo Silverio");
+		f3.setCargo("Analista de Engenharia de ti Senhor");
+		f3.setSalario(12000.0);
 		f1.mostrarDados();
 		f2.mostrarDados();
 		f3.mostrarDados();
 		//Chamando o método mostrarDados na classe funcionario;
 		// mostrarDados é chamado nos três funcionários;
-		
+f1.almentarSalario(10);
+f1.mostrarDados();
+//Chama um método da classe funcionario, que almenta o salario em um percentual. Depois chama o método que mostra os dados; 
 	}
 }
---
+++ ProjetoFuncionario/src/nucleo/Funcionario.java
@@ -0,0 +1,51 @@
+package nucleo;
+
+public class Funcionario {
+	private int numeroFuncional;
+	private String nome;
+	private String cargo;
+	private double salario;
+	//Os atributos da nossa classe Funcionario agora são privados;
+	public int getNumeroFuncional() {
+		return numeroFuncional;
+	}
+	public void setNumeroFuncional(int numeroFuncional) {
+		this.numeroFuncional = numeroFuncional;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+//Métodos geters (pegar) e seters (atribuir) gerados altomáticamente;
+	//Precionar alt + a, navegar com as setas até generate geters and seters. Precionar enter, depois precionar alt + a, e então precionar alt + r;
+	public void mostrarDados() {
+		//método que vai mostrar dados do funcionário na tela;
+		System.out.println("Informações dos colaboradores:");
+		System.out.println("Número funcional: " + numeroFuncional);
+		System.out.println("Nome do funcionário: " + nome);
+		System.out.println("Cargo do funcionário: " + cargo);
+		System.out.println("Salário do funcionário: " + salario);
+		System.out.println();
+	}
+
+	public void almentarSalario(double percentual) {
+		//Método que serve para caucular um almento de salário do funcionário;
+		salario = salario + salario * percentual / 100;
+	}
+	
+}
